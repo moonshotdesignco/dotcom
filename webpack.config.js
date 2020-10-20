7df16cb5544a8054f78dc1bd8db4697da516d851
harry <moonshotdesignco@gmail.com>
2020-10-19 22:27:45 -0400

    formatting + syntax + favicons + whatnot

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,18 @@ module.exports = {
     entry: "./src/js/main.js",
     output: {
         filename: "main_pack.js",
-        path: __dirname + "/prod"
+        path: __dirname + "/prod",
     },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"]
+                }
+            }
+        }]
+    }
 };
