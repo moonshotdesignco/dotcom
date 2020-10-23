ed593fd4ba7a2447dff356e94a29ab40320eca46
harry <moonshotdesignco@gmail.com>
2020-10-23 08:12:06 -0400

    src

---
+++ src/js/foldToggle.js
@@ -0,0 +1,54 @@
+const sBtn = document.querySelector("button.fold-toggle#see");
+const lBtn = document.querySelector("button.fold-toggle#learn");
+const rBtn = document.querySelector("button.fold-toggle#reach");
+
+const sCont = document.querySelector(".fold-element#see-content");
+const lCont = document.querySelector(".fold-element#learn-content");
+const rCont = document.querySelector(".fold-element#reach-content");
+
+const o = {
+    animFadeOut: function(elem, dur) {
+        elem.animate([{opacity: 1}, {opacity: 0}], {duration: dur});
+        return this;
+    },
+    animFadeIn: function(elem, dur) {
+        elem.animate([{opacity: 0}, {opacity: 1}], {duration: dur});
+        return this;
+    },
+    expTog: function(elem) {
+        elem.classList.toggle("expanded");
+        return this;
+    },
+    curTog: function(elem) {
+        elem.classList.toggle("current");
+        return this;
+    },
+    expRemove: function(elem, wait) {
+        setTimeout(function() {elem.classList.remove("expanded");}, wait);
+        return this;
+    },
+    curRemove: function(elem) {
+        elem.classList.remove("current");
+        return this;
+    },
+    cBtnShow: function(elem) {
+        elem.querySelector("#closeBtn").style.display = "unset";
+        return this;
+    },
+    cBtnHide: function(elem) {
+        elem.querySelector("#closeBtn").style.display = "none";
+        return this;
+    }
+};
+
+sBtn.onclick = function() {
+    sBtn.classList.contains("current") ? o.curRemove(sBtn).cBtnHide(sBtn).animFadeOut(sCont, 300).expRemove(sCont, 250) : o.curTog(sBtn).cBtnShow(sBtn).expTog(sCont).animFadeIn(sCont, 600);
+};
+
+lBtn.onclick = function() {
+    lBtn.classList.contains("current") ? o.curRemove(lBtn).cBtnHide(lBtn).animFadeOut(lCont, 300).expRemove(lCont, 250) : o.curTog(lBtn).cBtnShow(lBtn).expTog(lCont).animFadeIn(lCont, 600);
+};
+
+rBtn.onclick = function() {
+    rBtn.classList.contains("current") ? o.curRemove(rBtn).cBtnHide(rBtn).animFadeOut(rCont, 300).expRemove(rCont, 250) : o.curTog(rBtn).cBtnShow(rBtn).expTog(rCont).animFadeIn(rCont, 600);
+};
---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+import "./themeSwitcher";
+import "./foldToggle";
